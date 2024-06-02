cc169eded2c2828c4013633d6ba4bd06735594b3
Anton <augustaihenvald@gmail.com>
2024-06-02 18:48:40 +0300

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Ackerman(int n, int m)
+{
+    if(n == 0)
+    {
+        return m+1;
+    }
+    else if(m==0)
+    {
+        return Ackerman(n-1, 1);
+    }
+    else
+    {
+        return Ackerman(n-1, Ackerman(n, m-1));
+    }
+}
+
+Console.WriteLine(Ackerman(4, 0));
