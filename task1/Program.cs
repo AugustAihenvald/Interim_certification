9c6a08548a07ac55d101c12dba11f4b347d4417c
Anton <augustaihenvald@gmail.com>
2024-06-02 18:30:05 +0300

    task1 Done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintNumbersFromMtoN(int m, int n)
+{
+    if (m <= 0 || n <= 0)
+    {
+        Console.WriteLine("Одно из чисел является не натуральным!");
+    }
+    else if (m < n)
+    {
+        Console.Write($"{m}, ");
+        PrintNumbersFromMtoN(m + 1, n);
+    }
+    else if (m >= n)
+    {
+        Console.Write($"{m}, ");
+        if(m==n) return;
+        PrintNumbersFromMtoN(m - 1, n);
+    }
+
+}
+
+int m = 10;
+int n = 40;
+PrintNumbersFromMtoN(m, n);
